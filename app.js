36faf48919267822368c46cb74a6a6faa5b89335
Sandhaya Patel <56769455+Atharvpatel21@users.noreply.github.com>
2020-06-15 21:22:57 +0530

    catme and lodash used in app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const catMe = require("cat-me");
+const lodash = require("lodash");
+
+console.log(lodash.add(5, 6));
+
+
+// console.log(catMe());
+
+
+
+app.listen(3000, function(){
+    console.log("The server has started up.");
+   
+
+});
